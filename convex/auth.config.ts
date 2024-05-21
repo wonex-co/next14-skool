5d6d6dd88c3bf78c559ddd10e13c00a84e2f00ea
Felipe A. de Almeida <felipe.almeida@thinkam.net>
2024-05-21 15:15:19 -0300

    configuração via environment

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://clerk.rekreio.com.br/",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ]
